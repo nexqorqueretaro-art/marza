a0d4d8d85d0160b23937400f2cc69755bcc84199
nexqorqueretaro-art <nexqorqueretaro@gmail.com>
2026-07-31 13:49:50 -0600

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzSok6Dvfb-s5GBmO-TV5Jbzco0O_VTqEEaxsuojeWh37VACY6pHI7SnvCxrNaIULbWLA/exec",
-  BRAND_NAME: "MARZA BIENES RAÍCES"
-};
